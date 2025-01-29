8aacebb639e152bbb22ef9abebfcb8b0727d8fe5
rajesh03333 <rajeshgajula.1434@gmail.com>
2025-01-29 21:11:08 +0530

    font edited

--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -4,29 +4,35 @@ const Name = () => {
   return (
     <div className="min-h-screen bg-gray-100 p-6">
       <div className="max-w-5xl mx-auto bg-white p-6 rounded-2xl shadow-md border">
-        <div className="text-center">
-          <h1 className="text-lg font-bold">CVR COLLEGE OF ENGINEERING</h1>
-          <p className="text-sm">An UGC Autonomous Institution | Affiliated to JNTUH</p>
-          <p className="text-sm">Vastunagar, Mangalpally (V), Ibrahimpatnam (M), Ranga Reddy District - 501510</p>
-          <h2 className="text-lg font-bold mt-4">AWARD LIST (LABORATORY)</h2>
-        </div>
+      <div className="flex justify-center items-center border w-full h-23">
+       <div className="text-center border-r w-[820px] h-full">
+        <h1 className="text-2xl font-bold font-serif ">CVR COLLEGE OF ENGINEERING</h1>
+        <p className="text-lg font-semibold font-serif">An UGC Autonomous Institution Affiliated to JNTUH</p>
+        <p className="text-sm font-semibold font-serif">Vastunagar, Mangalpally (V), Ibrahimpatnam (M), Ranga Reddy District - 501510</p>
+      </div>
+      <div className="flex flex-col justify-center items-center w-[180px] h-full">
+        <h1 className="text-center text-xl font-bold font-serif ">College Code</h1>
+        <h1 className="text-2xl font-bold font-serif ">B8</h1>
+      </div>
+      </div>
 
         
 
-        <div className="flex flex-col gap-2 mt-6">
-          <div className="flex gap-7 w-full">
+        <div className="flex flex-col gap-2 mt-4">
+        <h2 className="text-lg font-bold mt-2 mb-3 text-center underline ">AWARD LIST (LABORATORY)</h2>
+          <div className="flex gap-5 w-full">
             <div>
-            <label className=" inline text-sm font-semibold ">Name of Exam:</label>
+            <label className=" inline text-sm font-semibold font-serif ">Name of Exam:</label>
             <input
               type="text"
-              className="text-sm w-[450px] border-b focus:outline-none border-black inline-block"
+              className="text-sm w-[440px] border-b focus:outline-none border-black inline-block"
             />
             </div>
             <div className="flex ">
-            <label className=" inline text-sm font-semibold ">(Reg./Supp)Month:</label>
+            <label className=" inline text-sm font-semibold font-serif ">(Reg./Supp)Month:</label>
             <input
               type="text"
-              className="  pl-2 text-sm w-[200px] border-b focus:outline-none border-black inline-block"
+              className="  pl-2 text-sm w-[180px] border-b focus:outline-none border-black inline-block"
             />
             <label className=" inline text-sm font-semibold ">20</label>
             <input
@@ -36,12 +42,12 @@ const Name = () => {
             </div>
           </div>
           <div className="mt-3">
-            <label className=" inline text-sm font-semibold">Branch & Section:</label>
+            <label className=" inline text-sm font-semibold font-serif ">Branch & Section:</label>
             <input
               type="text"
-              className="pl-2 text-sm w-[670px] border-b focus:outline-none border-black inline-block"
+              className="pl-2 text-sm w-[650px] border-b focus:outline-none border-black inline-block"
             />
-            <label className=" pl-2 inline text-sm font-semibold">Regulation:</label>
+            <label className=" pl-2 inline text-sm font-semibold font-serif  ">Regulation:</label>
             <input
               type="text"
               className="pl-2 text-sm w-[80px] border-b focus:outline-none border-black inline-block"
@@ -49,21 +55,21 @@ const Name = () => {
           </div>
           <div className="flex mt-3 gap-2">
           <div>
-            <label className="text-sm font-semibold">Name of Lab:</label>
+            <label className="text-sm font-semibold font-serif ">Name of Lab:</label>
             <input
               type="text"
-              className="pl-2 text-sm w-[400px] border-b focus:outline-none border-black inline-block"
+              className="pl-2 text-sm w-[380px] border-b focus:outline-none border-black inline-block"
             />
           </div>
           <div>
-            <label className="text-sm font-semibold">Date of Examination:</label>
+            <label className="text-sm font-semibold font-serif ">Date of Examination:</label>
             <input
               type="text"
-              className="pl-2 text-sm w-[130px] border-b focus:outline-none border-black inline-block"
+              className="pl-2 text-sm w-[150px] border-b focus:outline-none border-black inline-block"
             />
           </div>
           <div>
-            <label className="text-sm font-semibold">Max.Marks:</label>
+            <label className="text-sm font-semibold font-serif ">Max.Marks:</label>
             <input
               type="text"
               className="pl-2 text-sm w-[60px] border-b focus:outline-none border-black inline-block"
@@ -71,25 +77,25 @@ const Name = () => {
           </div>
           </div>
           <div className=" gap-1 flex mt-3 ">
-            <label className="text-sm font-semibold">Name & College of External Examiner:</label>
+            <label className="text-sm font-semibold font-serif ">Name & College of External Examiner:</label>
             <input
               type="text"
-              className="pl-2 text-sm w-[700px] border-b focus:outline-none border-black inline-block"
+              className="pl-2 text-sm w-[670px] border-b focus:outline-none border-black inline-block"
             />
           </div>
           <div className="flex gap-1 mt-3">
-            <label className="text-sm font-semibold">Name of Internal Examiner:</label>
+            <label className="text-sm font-semibold font-serif ">Name of Internal Examiner:</label>
             <input
               type="text"
-              className="pl-2 text-sm w-[752px] border-b focus:outline-none border-black inline-block"
+              className="pl-2 text-sm w-[740px] border-b focus:outline-none border-black inline-block"
             />
           </div>
         </div>
         <div className="mt-4 text-sm flex">
-          <p className="font-bold">Note:</p>
+          <p className="font-bold font-serif ">Note:</p>
           <ol className="ml-6">
-            <li>1) Please enter the marks in the serial order of the Hall Ticket Numbers of the students.</li>
-            <li>2) The award list must be submitted to the Controller of Examinations along with the Statement of Attendance.</li>
+            <li className="font-serif ">1) Please enter the marks in the serial order of the Hall Ticket Numbers of the students.</li>
+            <li className="font-serif ">2) The award list must be submitted to the Controller of Examinations along with the Statement of Attendance.</li>
           </ol>
         </div>
         <table className="w-full text-sm border-collapse border border-gray-300 mt-6">
@@ -193,7 +199,7 @@ const Name = () => {
           </tbody>
         </table>
 
-        <button className="mt-6 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
+        <button className="mt-6 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 items-center ml-110">
           Submit
         </button>
       </div>
